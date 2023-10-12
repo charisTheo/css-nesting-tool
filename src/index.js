df1c7fdb4f1810334e58cbfe0164cb391a58e834
Harry Theo <harrytheo@google.com>
2023-10-12 14:46:46 +0100

    Refactoring. Added file size comparison. Added copy button. Added example CSS file button.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,106 @@
-import '@material/web/button/filled-button.js';
-import '@material/web/button/filled-tonal-button.js';
-import '@material/web/textfield/filled-text-field';
-import '@material/web/icon/icon';
+import '@material/web/button/filled-button'
+import '@material/web/button/filled-tonal-button'
+import '@material/web/textfield/filled-text-field'
+import '@material/web/icon/icon'
 import '@material/web/progress/linear-progress'
+import '@material/web/elevation/elevation'
+import '@material/web/checkbox/checkbox'
+import '@material/web/divider/divider'
+import '@material/web/fab/fab'
 
-import { getMinifiedCSS } from './utils.js'
+import { getMinifiedCSS, textToKBs } from './utils.js'
+
+// TODO add CSS file upload button
+
+const EXAMPLE_CSS_FILE_URL = 'https://www.w3.org/StyleSheets/Core/Modernist'
+
+async function onSubmit(fileUrl) {
+  showLoadingIndicator()
+  await yieldToMain();
+
+  if (!fileUrl) {
+    fileUrl = EXAMPLE_CSS_FILE_URL
+  }
+  const [styleSheet, cssText] = await fetchAndCreateStylesheet(fileUrl);
+  const minifyEnabled = document.querySelector('#minify-css-checkbox').checked
+  const minifiedCSS = getMinifiedCSS(styleSheet, minifyEnabled)
+  
+  displayResults(cssText, minifiedCSS)
+  hideLoadingIndicator()
+}
 
 document.querySelector('#css-file-form').addEventListener('submit', async e => {
   e.preventDefault()
-  document.querySelector('md-linear-progress').style.display = 'block'
-  await new Promise(res => setTimeout(res, 0))
-  
   const fileUrl = document.querySelector("md-filled-text-field").value
-  const response = await fetch(fileUrl)
+  onSubmit(fileUrl)
+});
+
+document.querySelector('#example-button').addEventListener('click', () => onSubmit())
+
+async function fetchAndCreateStylesheet(url) {
+  const response = await fetch(url)
   const cssText = await response.text()
   const styleSheet = new CSSStyleSheet()
   styleSheet.replaceSync(cssText)
-  const minifiedCSS = getMinifiedCSS(styleSheet)
+  return [styleSheet, cssText]
+}
 
+function displayResults(before, after) {
   const nestedCSSContainer = document.querySelector('#nested-css-container')
   nestedCSSContainer.removeAttribute('hidden')
-  nestedCSSContainer.querySelector('#textarea-before').value = cssText
-  nestedCSSContainer.querySelector('#textarea-after').value = minifiedCSS
-  
-  document.querySelector('md-linear-progress').style.display = 'none'
-});
+  nestedCSSContainer.querySelector('#textarea-before').value = before
+  nestedCSSContainer.querySelector('#textarea-after').value = after
+  // show character length comparison - assuming UTF-8 encoding - 1 character = 1 byte
+  nestedCSSContainer.querySelector('.before .file-weight').textContent = `Original: ${textToKBs(before)} KBs`
+  nestedCSSContainer.querySelector('.after .file-weight').textContent = `With Nesting: ${textToKBs(after)} KBs`
 
-// TODO Show side-by-side comparison
-// TODO compare KBs
+  const copyButton = nestedCSSContainer.querySelector('.after #copy-button')
+  copyButton.addEventListener('click', () => {
+    const copied = copyToClipboard(after)
+    if (copied) {
+      const copyButtonIcon = copyButton.querySelector('md-icon')
+      const currentIcon = copyButtonIcon.innerText
+      copyButtonIcon.innerText = 'done'
+      copyButton.style.setProperty('--md-fab-container-color', '#4CAF50')
+      
+      setTimeout(() => {
+        copyButtonIcon.innerText = currentIcon
+        copyButton.style.removeProperty('--md-fab-container-color')
+      }, 3000);
+    }
+  })
+}
+
+function showLoadingIndicator() {
+  document.querySelector('md-linear-progress').style.display = 'block'
+}
+
+function hideLoadingIndicator() {
+  document.querySelector('md-linear-progress').style.display = 'none'
+}
+
+function yieldToMain() {
+  if ('scheduler' in window && 'yield' in scheduler) {
+    return scheduler.yield();
+  }
+  return new Promise(res => setTimeout(res, 0))
+}
+
+async function copyToClipboard (text) {
+  if (typeof window === 'undefined') {
+    return false
+  }
+  try {
+    await navigator.clipboard.writeText(text)
+    return true
+  } catch (_) {
+    // Async Clipboard API is not supported
+    const el = document.createElement('textarea')
+    el.value = text
+    document.body.appendChild(el)
+    el.select()
+    const result = document.execCommand('copy')
+    document.body.removeChild(el)
+    return result !== 'unsuccessful'
+  }
+}
