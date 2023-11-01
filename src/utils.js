40736695754770126e74307f0542160d43f0436d
Harry Theo <harrytheo@google.com>
2023-11-01 16:34:25 +0000

    Improved minification. Added support for @layer, @page, @property, @container and @scope.

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,9 @@
-// TODO Add support for Container queries
+// TODO create getCssText and getSelector helper functions for @property, @page, @container
+// TODO nest pseudo elements/selectors, i.e. :hover, :active, :before
 const NON_SELECTOR_RULE_TYPES = {
+  0: { identifier: '@property', valueKey: 'cssText' },
   4: { identifier: '@media', valueKey: 'conditionText' },
+  6: { identifier: '@page', valueKey: 'cssText' },
   7: { identifier: '@keyframes', valueKey: 'name' },
   8: { identifier: '', valueKey: 'cssText' },
   12: { identifier: '@supports', valueKey: 'conditionText' },
@@ -8,25 +11,74 @@ const NON_SELECTOR_RULE_TYPES = {
 
 function mapCssTextInSelectors(rule, currentLevel) {
   if (!rule.selectorText) {
-    // It's a @rule
+  // It's a @rule
     if (NON_SELECTOR_RULE_TYPES[rule.type]) {
+
+      //! @container, @layer, @scope and @property rules are all of type '0'
+
+      //? Container Queries
+      if (rule instanceof CSSContainerRule) {
+        const ruleName = `@container ${rule.containerQuery}`
+        currentLevel[ruleName] = {parentType: rule.type}
+        Array.from(rule.cssRules).map(r => {
+          mapCssTextInSelectors(r, currentLevel[ruleName]);
+        })
+        return
+      }
+
+      //? Scope rules
+      if (rule instanceof CSSScopeRule) {
+        //! console.log(rule) throws an error: https://bugs.chromium.org/p/chromium/issues/detail?id=1498448
+        const ruleName = `@scope (${rule.start})${rule.end ? ` to (${rule.end})` : ''}`
+        currentLevel[ruleName] = {parentType: rule.type}
+        Array.from(rule.cssRules).map(r => {
+          mapCssTextInSelectors(r, currentLevel[ruleName]);
+        })
+        return
+      }
+      
+      //? Layer order
+      if (rule instanceof CSSLayerStatementRule) {
+        currentLevel['cssText'] = (currentLevel['cssText'] || '') + rule.cssText.replaceAll(', ', ',')
+        return
+      }
+
+      //? Layer block rule
+      if (rule instanceof CSSLayerBlockRule) {
+        const ruleName = `@layer ${rule.name}`
+        currentLevel[ruleName] = {parentType: rule.type}
+        Array.from(rule.cssRules).map(r => {
+          mapCssTextInSelectors(r, currentLevel[ruleName]);
+        })
+        return
+      }
+      
       const { identifier, valueKey } = NON_SELECTOR_RULE_TYPES[rule.type]
-      currentLevel[`${identifier} ${rule[valueKey]}`] = {}
-      if (!rule.cssRules) {
-        console.log('rule', rule)
+
+      if (identifier === '@page') {
+        currentLevel[identifier] = {cssText: rule[valueKey].replaceAll(/(@page|{|})/g, '').trim()}
+      } else if (identifier === '@property') {
+        currentLevel[`${identifier} ${rule.name}`] = {cssText: rule[valueKey].replaceAll(/(@property.*{|})/g, '').trim()}
       } else {
-          Array.from(rule.cssRules).map(r => {
+        currentLevel[`${identifier} ${rule[valueKey]}`] = {parentType: rule.type}
+      }
+
+      if (rule.cssRules) {
+        Array.from(rule.cssRules).map(r => {
           mapCssTextInSelectors(r, currentLevel[`${identifier} ${rule[valueKey]}`]);
         })
       }
     } else {
-      // TODO debug only - What other rule types are there
-      console.log('👨‍💻 | mapCssTextInSelectors | rule:', rule);
+      console.warn('⚠️ | Uncaptured CSS rule | This rule will be omitted from the CSS output!', rule);
     }
     return
   }
-  // * in minified CSS, splitting with space still works!
-  const selectors = rule.selectorText.split(/>|\s|\+/).filter(s => s);
+
+  // * splitting between '>', '+' and space characters
+  // if input is minified CSS still works because it is firstly parsed into a StyleSheet that adds spaces by default
+  const selectors = rule.selectorText.split(/\s/).filter(s => s);
+  // TODO if there is a > character, search for [parent] as in [parent] > [child] and add a new rule
+  // i.e. currentLevel[parent] = {[child]: cssText}
   const ruleTopSelector = selectors[0];
 
   // no nesting needed as there is not second element in current selector
@@ -58,6 +110,10 @@ function cssTextMapToString(object, isNested = false, minifyEnabled) {
   const keys = Object.keys(object)
   
   return keys.map(k => {
+    if (k === 'parentType') {
+      return ''
+    }
+
     var skipNesting = false
     // If current selector doesn't have any CSS rules AND is not a media query or keyframes declaration
     if (!object[k]?.cssText && k.trim().indexOf('@') !== 0) {
@@ -66,18 +122,28 @@ function cssTextMapToString(object, isNested = false, minifyEnabled) {
       skipNesting = true
     }
 
-    // TODO remove nest character for media queries
-    /**
-     * @media (prefers-color-scheme: light) {
-     *   & .form-select {
-     */
+    if (object[k]?.cssText === '' && Object.keys(object[k] || {}).length < 2) {
+      // * skip empty CSS rules with no declarations
+      return ''
+    }
+
+    // skip nest character (&) for all @ CSS rules, i.e.  `@media (min-height: 768px) { & .form-select {...`
+    if (NON_SELECTOR_RULE_TYPES[object?.parentType]?.identifier) {
+      isNested = false
+    }
 
     if (k === 'cssText') {
-      return minifyEnabled 
-        ? object[k].trim().replaceAll(': ', ':') 
-        : object[k].trim().replaceAll('; ', `;\n${isNested ? '    ' : '  '}`)
+      return (
+        minifyEnabled 
+          // remove spaces  i.e. rgb(255, 228, 253) -> rgb(255,228,253)
+          // TODO remove 0's from floats i.e. rgb(0 0 0 / 0.5) -> rgb(0 0 0 / .5)
+          ? object[k].trim().replaceAll(/(?<=(\d,|:|;))\s/g, '')
+          // otherwise add new line characters between declarations
+          : object[k].trim().replaceAll('; ', `;\n${isNested ? '    ' : '  '}`)
+      )
+
     } else {
-      return `${addNestCharacter(isNested, minifyEnabled)}${addSelector(k, minifyEnabled)}${skipNesting ? '' : openBrackets(isNested, minifyEnabled)}${cssTextMapToString(object[k], !skipNesting, minifyEnabled).join('')}${skipNesting ? '' : closeBrackets(isNested, minifyEnabled)}`
+      return `${addNestCharacter(isNested, minifyEnabled)}${addSelector(k, minifyEnabled, skipNesting)}${skipNesting ? '' : openBrackets(isNested, minifyEnabled)}${cssTextMapToString(object[k], !skipNesting, minifyEnabled).join('')}${skipNesting ? '' : closeBrackets(isNested, minifyEnabled)}`.replaceAll(';}', '}')
     }
   })
 }
@@ -91,7 +157,6 @@ function cssTextMapToString(object, isNested = false, minifyEnabled) {
 export function getMinifiedCSS(styleSheet, minifyEnabled) {
   const TOP_SELECTORS_MAP = {};
 
-  // const styleSheet = document.styleSheets[0];
   const rules = Array.from(styleSheet?.cssRules || styleSheet?.rules);
   // console.log('🪲 | rules:', rules);
 
@@ -105,8 +170,12 @@ export function getMinifiedCSS(styleSheet, minifyEnabled) {
 function addNestCharacter(isNested, minifyEnabled) {
   return isNested ? (minifyEnabled ? '& ' : '\n\n  & ') : ''
 }
-function addSelector(selector, minifyEnabled) {
-  return selector + (minifyEnabled ? '' : ' ')
+function addSelector(selector, minifyEnabled, isNested) {
+  // remove space from ': ' in '@media screen and (min-width: 768px)'
+  return (minifyEnabled 
+    ? selector.replaceAll(/(?<=:)\s/g, '')
+    : selector + ' ')
+    + (isNested ? ' ' : '')
 }
 function openBrackets(isNested, minifyEnabled) {
   return minifyEnabled ? '{' : `{\n  ${isNested ? '  ' : ''}`
@@ -117,7 +186,7 @@ function closeBrackets(isNested, minifyEnabled) {
 
 /**
  * @param {String} text - i.e. 'abababababababa'
- * @returns {String} - '15'
+ * @returns {String} - '15' (KBs)
  */
 export function textToKBs(text) {
   return splitThousandsWithComma((text.length / 1024).toFixed(2))
